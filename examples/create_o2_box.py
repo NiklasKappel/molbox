e6ee6d5f88e377e749121f3da496e7ca8232ebdb
Niklas Kappel <niklas.kappel@kit.edu>
2024-04-25 14:00:20 +0200

    Update example

--- examples/create_o2_box.py
+++ examples/create_o2_box.py
@@ -9,7 +9,7 @@ def main():
     box = Box([0, 0, 0], [10, 10, 10])
     o2_box = MolBox(box=box)
     o2_mol = create_3d_mol_from_smiles("O=O")
-    num_mols = 32
+    num_mols = 8
     for _ in range(num_mols):
         rotation = Rotation.random()
         new_mol = rotate_mol(o2_mol, rotation, "COM")
