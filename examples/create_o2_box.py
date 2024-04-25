27bee745101ade218512326241f58812feedefa5
Niklas Kappel <niklas.kappel@kit.edu>
2024-04-25 08:48:48 +0200

    Fix random rotations

--- examples/create_o2_box.py
+++ examples/create_o2_box.py
@@ -1,6 +1,7 @@
 from molbox import Box, MolBox
 from molbox.lammps_data import write_lammps_data
 from molbox.rdkit_3d_ops import create_3d_mol_from_smiles, rotate_mol
+from rdkit import Chem
 from scipy.spatial.transform import Rotation
 
 
@@ -10,9 +11,10 @@ def main():
     o2_mol = create_3d_mol_from_smiles("O=O")
     num_mols = 32
     for _ in range(num_mols):
-        rotation = Rotation.random(random_state=42)
+        rotation = Rotation.random()
         new_mol = rotate_mol(o2_mol, rotation, "COM")
         o2_box.add_mol_randomly(new_mol)
+    Chem.MolToMolFile(o2_box.mol, "examples/o2_box.mol")
     write_lammps_data(o2_box, "examples/o2_box.lmpdat")
 
 
